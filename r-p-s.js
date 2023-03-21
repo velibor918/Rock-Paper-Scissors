c13a6d12d8f1bc6f32377208b2ceb15e3b157764
velibor918 <velibor918@gmail.com>
2023-03-21 22:22:10 +0100

    Added function for player choice

--- r-p-s.js
+++ r-p-s.js
@@ -1,6 +1,6 @@
 /*  establish the rules RPS in array CHECK
-    create function for random computer choice
-    create prompt for player choice
+    create function for random computer choice CHECK
+    create prompt for player choice CHECK
     create function that plays one round of RPS
     create loop that plays RPS 5 times
     add win counters for both player and computer
@@ -12,7 +12,10 @@ function computerChoice(arr) {
     return rules[Math.floor(Math.random() * rules.length)]
 }
 
-console.log(computerChoice(rules))
-console.log(computerChoice())
-console.log(computerChoice(rules))
-console.log(computerChoice())
+function playerChoice() {
+    let pC = prompt("Let's play a game of Rock, Paper, Scissors!");
+    let pCString = pC.toString();
+    return pCString.charAt(0).toUpperCase() + pCString.slice(1).toLowerCase();
+}
+
+playerChoice();
