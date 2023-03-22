4b2174c3a6f8c148b96192708ffae56e0b43e8db
velibor918 <velibor918@gmail.com>
2023-03-22 14:42:14 +0100

    Added function that plays one round of RPS

--- r-p-s.js
+++ r-p-s.js
@@ -1,7 +1,7 @@
 /*  establish the rules RPS in array CHECK
     create function for random computer choice CHECK
     create prompt for player choice CHECK
-    create function that plays one round of RPS
+    create function that plays one round of RPS CHECK
     create loop that plays RPS 5 times
     add win counters for both player and computer
     make win counters increase incrementally */
@@ -18,4 +18,34 @@ function playerChoice() {
     return pCString.charAt(0).toUpperCase() + pCString.slice(1).toLowerCase();
 }
 
-playerChoice();
+function playRPS(playerChoice, computerChoice) {
+    if (playerChoice === "Rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats Rock."
+    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        return "You win! Rock beats Paper."
+    } else if (playerChoice === "Rock" && computerChoice === "Rock") {
+        return "It's a tie!"
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock."
+    } else if (playerChoice === "Paper" && computerChoice === "Paper") {
+        return "It's a tie!"
+    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beats Paper."
+    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+        return "You lose! Rock beats Scissors."
+    } else if (playerChoice === "Scissors" && computerChoice === "Scissors") {
+        return "It's a tie!"
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        return "You win! Scissors beats Paper."
+    } else {
+        return "Somehow we managed to fuck up Rock, Paper, Scissors..."
+    }
+}   
+
+console.log(playRPS(playerChoice(), computerChoice()));
+console.log(playRPS(playerChoice(), computerChoice()));
+console.log(playRPS(playerChoice(), computerChoice()));
+console.log(playRPS(playerChoice(), computerChoice()));
+console.log(playRPS(playerChoice(), computerChoice()));
+console.log(playRPS(playerChoice(), computerChoice()));
+console.log(playRPS(playerChoice(), computerChoice()));
